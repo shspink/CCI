81052b5e34c6900a099b45db31fae9035b7dc219
hsson <shspink2@gmail.com>
2018-08-22 20:11:36 +0900

    amend CCI1_8 code

--- CCI_1/CCI1_8.java
+++ CCI_1/CCI1_8.java
@@ -2,10 +2,10 @@ package practice;
 
 public class CCI1_8 {
 
-	static void push_zero_element_row_column(int[][] arr) {
+	void push_zero_element_row_column(int[][] arr) {
 
-		int[] zero_row = new int[arr.length / 65 + 1];
-		int[] zero_column = new int[arr[0].length / 65 + 1];
+		int[] zero_row = new int[arr.length / 32 + 1];
+		int[] zero_column = new int[arr[0].length / 32 + 1];
 
 		int row = 0, column = 0, convert_row = 0, convert_column;
 
@@ -16,12 +16,11 @@ public class CCI1_8 {
 
 				if (arr[row][column] == 0) {
 
-					convert_row = row / 65;
-					convert_column = column / 65;
+					convert_row = row / 32;
+					convert_column = column / 32;
 
-					zero_row[convert_row] = zero_row[convert_row] | (1 << (row % 65 - 1));
-					zero_column[convert_column] = zero_column[convert_column] | (1 << (column % 65 - 1));
-
+					zero_row[convert_row] = zero_row[convert_row] | (1 << (row % 32));
+					zero_column[convert_column] = zero_column[convert_column] | (1 << (column % 32));
 				}
 
 			}
@@ -31,7 +30,7 @@ public class CCI1_8 {
 		// ===================== Change elements ===================== // Search Count : row + column
 		for (row = 0; row < arr.length; row++) {
 
-			if ((zero_row[row / 65] & 1 << (row % 65 - 1)) == 1) {
+			if (((zero_row[row / 32] >> (row % 32)) & 1) == 1) {
 				for (column = 0; column < arr[0].length; column++) {
 					arr[row][column] = 0;
 				}
@@ -40,12 +39,13 @@ public class CCI1_8 {
 		}
 
 		for (column = 0; column < arr[0].length; column++) {
-			if ((zero_column[column / 65] & 1 << (column % 65 - 1)) == 1) {
+			if (((zero_column[column / 32] >> (column % 32)) & 1) == 1) {
 				for (row = 0; row < arr.length; row++) {
 					arr[row][column] = 0;
 				}
 			}
 		}
+
 	}
 
 }
