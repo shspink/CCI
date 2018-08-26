05d51e00560bd0e1114c7c549cc719f769ed65d2
hsson <shspink2@gmail.com>
2018-08-26 19:22:34 +0900

    Create CCI1_9.java

---
+++ CCI_1/CCI1_9.java
@@ -0,0 +1,21 @@
+//package All Kind of Java;
+
+public class CCI1_9 {
+	public boolean Find_String_rotate(String s1, String s2) {
+
+		if (s1.length() == s2.length() && s1.length() > 0) {
+			return isSubstring(s1 + s1, s2);
+		}
+
+		else
+			return false;
+
+	}
+	public boolean isSubstring(String s1, String s2) {
+		
+		// == make on my style
+		
+		return false;
+		
+	}
+}
