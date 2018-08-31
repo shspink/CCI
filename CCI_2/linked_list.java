51a5dc6a6209d7be0d318a02424de53f82fe07af
hsson <shspink2@gmail.com>
2018-08-31 17:50:06 +0900

    Create linked_list Class

---
+++ CCI_2/linked_list.java
@@ -0,0 +1,15 @@
+//package All Kind of Java;
+
+public class linked_list {
+	node head_node;
+
+	class node {
+		int element;
+		node next;
+	}
+	
+	void change_head_node(node c_node) {
+		this.head_node = c_node;
+	}
+	
+}
