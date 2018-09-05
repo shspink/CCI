89b4b305d0621fb4546b2f233c086ebc121b8739
hsson <shspink2@gmail.com>
2018-09-05 19:46:26 +0900

    amend CCI2_4.java code

--- CCI_2/CCI2_4.java
+++ CCI_2/CCI2_4.java
@@ -1,26 +1,49 @@
 //package All Kind of Java;
 
-import practice.linked_list.node;
+import All Kind of Java.linked_list.node;
 
 public class CCI2_4 {
 
 	void move_right_smaller_than_X_node(linked_list L_list, int X) {
-		node tmp_node = L_list.head_node, tmp_pre_node = L_list.head_node, smaller_than_X_head_node = null,
+		node tmp_node = L_list.head_node, tmp_pre_node = L_list.new node(), smaller_than_X_head_node = null,
 				smaller_than_X_tail_node = null;
+		tmp_pre_node.next = L_list.head_node;
 
-		if (tmp_node.element < X) {
-			if (tmp_node == L_list.head_node)
-				L_list.head_node = L_list.head_node.next;
-			else
-				tmp_pre_node.next = tmp_node.next;
-			
-			if (smaller_than_X_head_node == null)
-				smaller_than_X_head_node = tmp_node;
-			else
-				smaller_than_X_tail_node.next = tmp_node;
-			smaller_than_X_tail_node = tmp_node;
-			
+		while (tmp_node != null) {
+// === Check tmp_node element smaller than X 
+			if (tmp_node.element < X) {
+// === Check tmp_node is head_node 
+				if (tmp_node == L_list.head_node) {
+					L_list.head_node = L_list.head_node.next;
+					tmp_pre_node.next = L_list.head_node;
+				}
+
+				else
+					tmp_pre_node.next = tmp_node.next;
+// === Check small node list is empty 
+				if (smaller_than_X_head_node == null) {
+					smaller_than_X_head_node = tmp_node;
+					smaller_than_X_tail_node = tmp_node;
+				}
+
+				else {
+					smaller_than_X_tail_node.next = tmp_node;
+					smaller_than_X_tail_node = tmp_node;
+				}
+				smaller_than_X_tail_node.next = null;
+
+				tmp_node = tmp_pre_node.next;
+			}
+
+			else {
+				tmp_node = tmp_node.next;
+				tmp_pre_node = tmp_pre_node.next;
+			}
+
 		}
+// === Combine as One list 
+		tmp_pre_node.next = smaller_than_X_head_node;
 
 	}
+
 }
